175fe6f7c70368699a70163101ddf28018774e11
mapsedge <41483625+mapsedge@users.noreply.github.com>
2021-12-02 10:24:28 -0600

    Files added.

---
+++ keymap.src.js
@@ -0,0 +1,111 @@
+;
+/* 
+AUTHOR: Bill Morris
+DATE: 	2021-12-02
+
+No license.
+
+Be my guest. A useful list, with my compliments.
+
+-- Measure with a micrometer. Mark with a crayon. Cut with an axe. --
+
+*/
+
+var KEYUP_BACK_QUOTE = 192;
+var KEYUP_1 = 49;
+var KEYUP_2 = 50;
+var KEYUP_3 = 51;
+var KEYUP_4 = 52;
+var KEYUP_5 = 53;
+var KEYUP_6 = 54;
+var KEYUP_7 = 55;
+var KEYUP_8 = 56;
+var KEYUP_9 = 57;
+var KEYUP_0 = 48;
+var KEYUP_MINUS = 189;
+var KEYUP_EQUALS = 187;
+var KEYUP_BACK_SPACE = 8;
+var KEYUP_TAB = 9;
+var KEYUP_Q = 81;
+var KEYUP_W = 87;
+var KEYUP_E = 69;
+var KEYUP_R = 82;
+var KEYUP_T = 84;
+var KEYUP_Y = 89;
+var KEYUP_U = 85;
+var KEYUP_I = 73;
+var KEYUP_O = 79;
+var KEYUP_P = 80;
+var KEYUP_OPEN_BRACKET = 219;
+var KEYUP_CLOSE_BRACKET = 221;
+var KEYUP_BACK_SLASH = 220;
+var KEYUP_CAPS_LOCK = 20;
+var KEYUP_A = 65;
+var KEYUP_S = 83;
+var KEYUP_D = 68;
+var KEYUP_F = 70;
+var KEYUP_G = 71;
+var KEYUP_H = 72;
+var KEYUP_J = 74;
+var KEYUP_K = 75;
+var KEYUP_L = 76;
+var KEYUP_SEMICOLON = 186;
+var KEYUP_QUOTE = 222;
+var KEYUP_ENTER = 13;
+var KEYUP_SHIFT = 16;
+var KEYUP_Z = 90;
+var KEYUP_X = 88;
+var KEYUP_C = 67;
+var KEYUP_V = 86;
+var KEYUP_B = 66;
+var KEYUP_N = 78;
+var KEYUP_M = 77;
+var KEYUP_COMMA = 188;
+var KEYUP_PERIOD = 190;
+var KEYUP_SLASH = 191;
+var KEYUP_SHIFT = 16;
+var KEYUP_CONTROL = 17;
+var KEYUP_OS_KEY = 91;
+var KEYUP_ALT = 18;
+var KEYUP_SPACE = 32;
+var KEYUP_PRINTSCREEN = 44;
+var KEYUP_PAUSE = 19;
+var KEYUP_HOME = 36;
+var KEYUP_PAGE_UP = 33;
+var KEYUP_END = 35;
+var KEYUP_PAGE_DOWN = 34;
+var KEYUP_DELETE = 46;
+var KEYUP_INSERT = 45;
+var KEYUP_UP = 38;
+var KEYUP_LEFT = 37;
+var KEYUP_DOWN = 40;
+var KEYUP_RIGHT = 39;
+var KEYUP_NUM_LOCK = 144;
+var KEYUP_DIVIDE = 111;
+var KEYUP_MULTIPLY = 106;
+var KEYUP_SUBTRACT = 109;
+var KEYUP_ADD = 107;
+var KEYUP_ENTER = 13;
+var KEYUP_DECIMAL = 110;
+var KEYUP_NUMPAD0 = 96;
+var KEYUP_NUMPAD1 = 97;
+var KEYUP_NUMPAD2 = 98;
+var KEYUP_NUMPAD3 = 99;
+var KEYUP_NUMPAD4 = 100;
+var KEYUP_NUMPAD5 = 101;
+var KEYUP_NUMPAD6 = 102;
+var KEYUP_NUMPAD7 = 103;
+var KEYUP_NUMPAD8 = 104;
+var KEYUP_NUMPAD9 = 105;
+var KEYUP_F1 = 112;
+var KEYUP_F2 = 113;
+var KEYUP_F3 = 114;
+var KEYUP_F4 = 115;
+var KEYUP_F5 = 116;
+var KEYUP_F6 = 117;
+var KEYUP_F7 = 118;
+var KEYUP_F8 = 119;
+var KEYUP_F9 = 120;
+var KEYUP_F10 = 121;
+var KEYUP_F11 = 122;
+var KEYUP_F12 = 123;
